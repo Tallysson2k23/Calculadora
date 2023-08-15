d1eed47c9ceaeca8eee85dbfef137ecb59793d9d
Tallysson2k23 <tallysson.bldd@gmail.com>
2023-08-15 15:37:53 -0300

    Initial commit

---
+++ calculadora.js
@@ -0,0 +1,48 @@
+
+class Calculadora {
+  adicionar(x, y) {
+    return x + y;
+  }
+  subtracao(x, y) {
+    return x - y;
+  }
+
+  multiplicacao(x, y) {
+    return x * y;
+  }
+
+  divisao(x, y) {
+    return x / y;
+  }
+}
+
+const calculadora = new Calculadora();
+
+const prompt = require("prompt-sync")();
+
+const num1 = parseFloat(prompt("informe um numero: "));
+
+const operacao = prompt("Escohla uma das operaraçoes: + , - , / , *: ");
+
+const num2 = parseFloat(prompt("informe proximo numero: "));
+
+
+let mensagem = "";
+switch (operacao) {
+  case "+":
+    mensagem = calculadora.adicionar(num1, num2);
+    break;
+  case "-":
+    mensagem = calculadora.subtracao(num1, num2);
+    break;
+  case "*":
+    mensagem = calculadora.multiplicacao(num1, num2);
+    break;
+  case "/":
+    mensagem = calculadora.divisao(num1, num2);
+    break;
+}
+
+
+
+console.log(mensagem);
